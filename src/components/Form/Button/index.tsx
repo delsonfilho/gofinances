ab59e1cd74ab1a35af11b82f1d6e36b957007c07
Delson Filho <delson.filho@gmail.com>
2021-06-29 08:19:17 -0300

    novos arquivos

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -5,6 +5,7 @@ import { Container, Title } from "./styles";
 
 interface Props extends RectButtonProps {
   title: string;
+  onPress: () => void;
 }
 
 export function Button({ title, onPress, ...rest }: Props) {
