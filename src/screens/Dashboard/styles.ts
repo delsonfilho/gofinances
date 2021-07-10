cc5b6b0aa5c524cc20f6f37a6d2957540c1c4bb8
Delson Filho <delson.filho@gmail.com>
2021-07-10 00:51:38 -0300

    novas implementações

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -67,9 +67,7 @@ export const UserName = styled.Text`
   font-family: ${({ theme }) => theme.fonts.bold};
 `;
 
-export const LogoutButton = styled(BorderlessButton)`
-
-`;
+export const LogoutButton = styled(BorderlessButton)``;
 
 export const Icon = styled(Feather)`
   color: ${({ theme }) => theme.colors.secundary};
@@ -109,3 +107,9 @@ export const TransactionList = styled(
     paddingBottom: getBottomSpace(),
   },
 })``;
+
+export const LoadContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -31,10 +31,14 @@ interface FormData {
 }
 
 const schema = Yup.object().shape({
-  name: Yup.string().required("Nome é obrigatório"),
-  amount: Yup.number()
+  name: Yup
+  .string()
+  .required("Nome é obrigatório"),
+  amount: Yup
+  .number()
     .typeError("Informe um valor numérico")
-    .positive("O valor não pode ser negativo"),
+    .positive("O valor não pode ser negativo")
+    .required("O valor é obrigatório"),
 });
 
 import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
